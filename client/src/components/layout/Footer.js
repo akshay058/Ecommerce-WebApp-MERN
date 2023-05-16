4bc1a2a41db215f3270a9336c790c4cbc0693bf7
Akshay <jainakshay058@gmail.com>
2023-05-16 12:18:13 +0530

    added routes in react client

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h1 className="text-center">All Right Reserverd &copy; Bachcha Toys </h1>
+    </div>
+  );
+}
---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+export default function Pagenotfound() {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+    </Layout>
+  );
+}
