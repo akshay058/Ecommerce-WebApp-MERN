72378e47ba5d14284eddc84519c5c738ab2eaf69
Akshay <jainakshay058@gmail.com>
2023-05-17 19:21:29 +0530

    some change

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-dark text-light p-3">
-      <h1 className="text-center">All Right Reserverd &copy; Bachcha Toys </h1>
+      <h1 className="text-center">All Right Reserverd &copy; Ecommerce App </h1>
     </div>
   );
 }
