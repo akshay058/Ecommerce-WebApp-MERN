be81028fb4db53d5541add5fe47d4f23c2102108
Akshay <jainakshay058@gmail.com>
2023-09-12 14:47:18 +0530

    build of react

--- server.js
+++ server.js
@@ -25,22 +25,22 @@ app.use(express.json()); // instead of url.bodyparser this used in express
 app.use(morgan("dev")); // use see api data on console use morgan package
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-//PORT
-const PORT = process.env.PORT || 8080;
-
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+//PORT
+const PORT = process.env.PORT || 8080;
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1> Welcome to ecommerce app</h1>");
 });
 
+//run listen
 app.listen(PORT, () => {
   console.log(
     `Server is running on  ${process.env.DEV_MODE} mode on port : ${PORT}`
