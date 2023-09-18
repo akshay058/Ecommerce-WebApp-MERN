cba381f2ad686bacc4d337f21ad7289f9ff9970c
Akshay <jainakshay058@gmail.com>
2023-09-18 23:50:56 +0530

    create product

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import path from "path";
+import path from "path"; // for merging path of frontend and backend
 import { fileURLToPath } from "url";
 
 //config env
@@ -21,8 +21,8 @@ const app = express();
 connectDB();
 
 //es module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // create file
+const __dirname = path.dirname(__filename); // set file name in dirname
 
 //middlewares
 app.use(
@@ -32,13 +32,14 @@ app.use(
 );
 app.use(express.json()); // instead of url.bodyparser this used in express
 app.use(morgan("dev")); // use see api data on console use morgan package
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build"))); // join client build to directory
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+// join client path with directory
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
